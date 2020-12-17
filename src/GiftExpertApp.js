a383f1718c162bcd556931dc01b2856b98a23613
KBJ-ID <ideasdeveloperskbj@gmail.com>
2020-12-17 06:48:54 -0600

    Search-and-frontend
    Terminado la parte del frontend y el buscador de gifs.

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GiftGrid } from "./components/GiftGrid";
+
+export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  return (
+    <>
+      <h2>Expert Gift</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr></hr>
+
+      <ol>
+        {categories.map((category) => (
+          <GiftGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
